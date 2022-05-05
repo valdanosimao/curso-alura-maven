7072406602391c1f34a10351b44ebf5a3663c8b5
Valdano Simão <valdanosimao@gmail.com>
2022-05-05 13:57:13 -0300

    conhecendo os goals

--- src/test/java/loja/ProdutoTest.java
+++ src/test/java/loja/ProdutoTest.java
@@ -1,14 +1,12 @@
 package loja;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
